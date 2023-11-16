0962378d07efa6c347367f2d323542198e7cdcb1
Lucas <lm.spizzirri@gmail.com>
2023-11-16 15:13:21 -0300

    Req 1

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,7 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -9,5 +11,30 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    int qntAtividades = scanner.nextInt();
+    scanner.nextLine();
+
+    String[] nomeAtividades = new String[qntAtividades];
+    int[] pesoAtividades = new int[qntAtividades];
+    int somaDosPesos = 0;
+
+    for (int i = 0; i < qntAtividades; i++) {
+      System.out.println("Digite o nome da atividade" + (i + 1) + ":");
+      nomeAtividades[i] = scanner.nextLine();
+
+      System.out.println("Digite o peso da atividade" + (i + 1) + ":");
+      pesoAtividades[i] = scanner.nextInt();
+      somaDosPesos += pesoAtividades[i];
+      scanner.nextLine();
+    }
+
+    if (somaDosPesos != 100) {
+      System.out.println("A soma dos pesos deve ser 100");
+    } else {
+      System.out.println("Atividades cadastradas com sucesso!");
+    }
   }
 }
