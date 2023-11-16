57385e7bdefb281e0681a310ca18d0390a5976e5
Lucas <lm.spizzirri@gmail.com>
2023-11-16 16:09:29 -0300

    Fix Req 3 e Req 4

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -21,6 +21,7 @@ public class App {
     int[] pesoAtividades = new int[qntAtividades];
     int[] notaAtividades = new int[qntAtividades];
     int somaDosPesos = 0;
+    double somaNotas = 0;
 
     for (int i = 0; i < qntAtividades; i++) {
       System.out.println("Digite o nome da atividade" + (i + 1) + ":");
@@ -33,13 +34,27 @@ public class App {
 
       System.out.println("Digite sua nota obtida na atividade" + nomeAtividades[i] + ":");
       notaAtividades[i] = scanner.nextInt();
+      somaNotas += pesoAtividades[i] * notaAtividades[i];
       scanner.nextLine();
     }
 
     if (somaDosPesos != 100) {
-      System.out.println("A soma dos pesos deve ser 100");
+      System.out.println("A soma dos pesos é diferente de 100!");
     } else {
       System.out.println("Atividades cadastradas com sucesso!");
+    }
+
+    double notaFinal = somaNotas / 100;
+    if (notaFinal >= 85) {
+      System.out.println(
+          "Parabéns! Você alcançou"
+              + notaFinal
+              + "%! E temos o prazer de informar que você obteve aprovação!");
+    } else {
+      System.out.println(
+          "Lamentamos informar que, com base na sua pontuação alcançada neste período,"
+          + notaFinal
+              + "você não atingiu a pontuação mínima necessária para sua aprovação.");
     }
   }
 }
